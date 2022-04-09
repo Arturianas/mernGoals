97578d6302d7e78f3fb373cdb49443f30b71b2d4
Arturianas <arturasdomnenko@gmail.com>
2022-04-10 00:51:29 +0300

    commit message

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 const goalRoute = require('./routes/goalRoutes');
 const userRoute = require('./routes/userRoutes');
+const userRoute = require('../frontend/build/');
 
 
 
@@ -24,7 +25,7 @@ app.use('/api/users', userRoute);
 
 
 //Serve frontend
-
+__dirname = path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')));
